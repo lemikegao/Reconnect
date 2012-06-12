1f4f6a0375d8ec6c2824fed44cec947b162d35cb
Michael Gao <mgao.ucla@gmail.com>
2012-06-12 12:11:00 -0400

    + LoginViewController

---
+++ Reconnect/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Reconnect
+//
+//  Created by Michael Gao on 6/12/12.
+//  Copyright (c) 2012 Calimucho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Reconnect/LoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.m
+//  Reconnect
+//
+//  Created by Michael Gao on 6/12/12.
+//  Copyright (c) 2012 Calimucho. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
